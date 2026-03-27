a51b5ff2e01c3ff074c7d1309046c5b3c8b837b7
LuizVMaciel <luizvmaciel26@gmail.com>
2026-03-27 19:49:26 -0300

    Adicionado sistema de atribuição de classes/cargos para os jogadores

--- VersãoPython/CidadeDorme.py
+++ VersãoPython/CidadeDorme.py
@@ -1,10 +1,12 @@
+import random
 while True:
     #Set da lista e inicialização do programa
     jogadores = []
     print('Bem vindo ao Cidade Dorme')
     quantidadeInicialJogadores = int(input('Quantas pessoas irão jogar? '))
-    #Sistema de registro de jogadores
-    if (quantidadeInicialJogadores > 0):
+    #Validação de que o numero de jogadores seja positivo e maior que dois
+    if (quantidadeInicialJogadores > 2):
+        #Sistema de registro de jogadores
         jogadores.append(input('digite o seu nome: '))
         quantidadeInicialJogadores -= 1
         while(quantidadeInicialJogadores > 0):
@@ -12,7 +14,14 @@ while True:
             jogadores.append(input(f'digite o nome do jogador {quantidadeJogadoresJogando + 1}: '))
             quantidadeInicialJogadores -= 1
         print('todos jogadores foram registrados!')
+        #atribuição de classes/cargos para os jogadores 
+        cidadaos = jogadores.copy()
+        assassino = random.choice(cidadaos)
+        cidadaos.remove(assassino)
+        detetive = random.choice(cidadaos)
+        cidadaos.remove(detetive)
+        anjo = random.choice(cidadaos)
+        cidadaos.remove(anjo)
         break
-    #Validação de que o numero de pessoas seja positivo
     else:
         print('Digite um número válido')
