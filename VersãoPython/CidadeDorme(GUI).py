dd0f2f4ced259d46ec074a4630b33c96e98b42df
LuizVMaciel <luizvmaciel26@gmail.com>
2026-04-08 00:34:44 -0300

    Adicionado botão jogar e caixa de texto à tela inicial.

--- VersãoPython/CidadeDorme(GUI).py
+++ VersãoPython/CidadeDorme(GUI).py
@@ -1,4 +1,9 @@
 from tkinter import *
 janela = Tk()
 janela.title('Cidade Dorme')
+janela.configure(background = "#424242")
+botaoJogar = Button(janela, text = 'Jogar')
+botaoJogar.place(relx = 0.25, rely = 0.75, relwidth = 0.50, relheight = 0.1)
+textoBemVindo = Label(janela, text = 'Bem vindo ao Cidade Dorme!')
+textoBemVindo.place(relx = 0.1, rely = 0.25, relwidth = 0.8, relheight = 0.1)
 janela.mainloop()
